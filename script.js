65fc171a6611b4c46f83131c02716392bfa10052
shivam0088 <shivamhaldar2134@gmail.com>
2023-05-02 13:48:25 +0530

    .

--- script.js
+++ script.js
@@ -29,7 +29,6 @@ cursor.classList.remove('hover');
 });
 })
 
-// Sticky Navigation Menu JS Code
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
 console.log(scrollBtn);
